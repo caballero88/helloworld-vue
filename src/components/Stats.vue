5d80f8294f4155bee4bef6f629943794b1379ab6
chang.m7yx.yifengxica.net <chang.m7yx.yifengxica.net>
2019-10-10 14:45:23 +0900

    second

---
+++ src/components/Stats.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="stats">
+    <h1>A different component</h1>
+    <p>There are currently {{ countLinks }} links</p>
+
+    <button v-on:click="removeAllLinks">Remove All links</button>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations, mapActions } from 'vuex'
+
+export default {
+  name: 'Stats',
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'countLinks'
+    ]),
+  },
+  methods: {
+    ...mapMutations(['REMOVE_ALL']),
+    ...mapActions(['removeAll']),
+    removeAllLinks() {
+      this.removeAll().then(() => {
+        this.msg = 'They have been removed'
+      });
+    }
+  }
+}
+</script>
